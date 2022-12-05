ad076e8cee19fbba85db1472b2a7bac08688e30f
lilnpuma <manump@terpmail.umd.edu>
2022-12-05 18:55:04 -0500

    Added controller cpp and hpp files. Added controller node.

---
+++ simple_turtle/include/simple_turtle/controller.hpp
@@ -0,0 +1,80 @@
+/**
+ * @file controller.hpp
+ * @author Manu Pillai (manump@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-05
+ *
+ * MIT License
+ * Copyright (c) 2022 Manu Pillai
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#ifndef CONTROLLER_HPP_
+#define CONTROLLER_HPP_
+#include <chrono>
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+#define RAD2DEG (180.0 / M_PI)
+#define DEG2RAD (M_PI / 180.0)
+
+namespace Simpleturt {
+class Simpleturt : public rclcpp::Node {
+public:
+  /// @brief Constructor for the Simpleturt class
+  /// @param name
+  Simpleturt(std::string name);
+  /// @brief Destructor for the Simpleturt class
+  ~Simpleturt();
+  /// @brief Callback function for the laser scan subscriber
+  /// @param msg
+  void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  /// @brief Move the turtlebot forward
+  void moveForward();
+  /// @brief  Rotate the turtlebot in the anticlockwise direction
+  void rotate();
+  /// @brief
+  /// @param linear
+  /// @param angular
+  void setVelocity(float linear, float angular);
+  /// @brief
+  void stop();
+
+private:
+  /// @brief
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+  /// @brief
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr sub_;
+  /// @brief
+  geometry_msgs::msg::Twist vel_;
+  /// @brief
+  // cmd_vel publisher timer
+  rclcpp::TimerBase::SharedPtr pub_timer_;
+  /// @brief
+  // rotate timer
+  rclcpp::TimerBase::SharedPtr rotate_timer_;
+  bool reverse_;
+};
+}; // namespace Simpleturt
+
+#endif /* CONTROLLER_HPP_ */
---
+++ simple_turtle/src/controller.cpp
@@ -0,0 +1,107 @@
+/**
+ * @file controller.cpp
+ * @author Manu Pillai (manump@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-05
+ *
+ * MIT License
+ * Copyright (c) 2022 Manu Pillai
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include "controller.hpp"
+
+#include <numeric>
+
+testudo::Simpleturt::Simpleturt(std::string name = "SimpleTestudo") : Node(name) {
+  // Create a publisher object.
+  pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+  // Create a subscriber object.
+  sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+      "/scan", 10,
+      std::bind(&Simpleturt::scanCallback, this, std::placeholders::_1));
+  // Create a timer.
+  pub_timer_ = this->create_wall_timer(std::chrono::milliseconds(10),
+                                       std::bind(&Simpleturt::moveForward, this));
+  rotate_timer_ = this->create_wall_timer(std::chrono::milliseconds(10),
+                                          std::bind(&Simpleturt::rotate, this));
+  // Cancel the rotation timer. Start with only the forward motion.
+  rotate_timer_->cancel();
+  RCLCPP_INFO(this->get_logger(), "Simpleturt node has been started");
+}
+testudo::Simpleturt::~Simpleturt() {
+  stop();
+  RCLCPP_INFO(this->get_logger(), "Simpleturt node has been stopped");
+}
+void testudo::Simpleturt::scanCallback(
+    const sensor_msgs::msg::LaserScan::SharedPtr msg) {
+  // Get average of the left 90 degree range
+  auto front =
+      std::vector<float>(msg->ranges.begin(), msg->ranges.begin() + (40));
+  // Get average of the right 90 degree range
+  front.insert(front.end(), msg->ranges.end() - (40), msg->ranges.end());
+  // Get average of the front 180 degree range
+  auto min_range = *std::min_element(front.begin(), front.end());
+  // Check if the minimum range is less than 0.65 m
+  if (min_range < 0.65) {
+    // Stop the turtlebot
+    if (rotate_timer_->is_canceled()) {
+      stop();
+      // Start rotating
+      pub_timer_->cancel();
+      rotate_timer_->reset();
+    }
+  } else {
+    // Start moving forward
+    if (pub_timer_->is_canceled()) {
+      stop();
+      pub_timer_->reset();
+      rotate_timer_->cancel();
+    }
+  }
+}
+void testudo::Simpleturt::moveForward() {
+  // Create a message object.
+  setVelocity(0.5, 0.0);
+  // Publish the message.
+  pub_->publish(vel_);
+}
+void testudo::Simpleturt::rotate() {
+  // Create a message object.
+  setVelocity(0.0, 0.5);
+  // Publish the message.
+  pub_->publish(vel_);
+}
+void testudo::Simpleturt::setVelocity(float linear, float angular) {
+  // Set the Velocity of the robot to the geometry message.
+  geometry_msgs::msg::Twist msg;
+  msg.linear.x = linear;
+  msg.angular.z = angular;
+  // Set the message to the class variable.
+  vel_ = msg;
+}
+void testudo::Simpleturt::stop() {
+  // Stop the robot.
+  setVelocity(0.0, 0.0);
+  // Publish the message.
+  pub_->publish(vel_);
+}
